022dcb2edf585181a5ef2a592592c013213ec364
ThanosTsoukalas <tsouthanos@gmail.com>
2024-08-04 01:58:50 +0300

    Added the info panel

---
+++ ThanOS/Javascript Files/System/BatteryLevel.js
@@ -0,0 +1,10 @@
+//Used in workspace.html
+// If the user has a Desktop PC or iMac, this script also prevents the battery status 
+// from being displayed on the desktop.
+
+if (navigator.getBattery) {
+    // if battery API is working
+}
+else {
+    // if battery API is not working
+}
