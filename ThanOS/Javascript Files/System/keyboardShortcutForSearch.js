f495f34f896152ed06434ead567604fd067201db
Thanos Tsoukalas <tsouthanos@gmail.com>
2024-08-24 23:46:50 +0300

    added further documentation

--- ThanOS/Javascript Files/System/keyboardShortcutForSearch.js
+++ ThanOS/Javascript Files/System/keyboardShortcutForSearch.js
@@ -1,26 +1,43 @@
 const searchInput = document.getElementById('search-window');
 const searchInputField = document.getElementById('search-input-field');
+
 var searchInputFilled = false;
 var searchInputValue = '';
 
+function checkInput(){
+
+}
+// The Shortcut for opening search is Ctrl + Shift + E, so we are checking that
+// first 
 document.addEventListener('keydown', e =>{
     if(e.ctrlKey && e.shiftKey && e.key.toLowerCase() === 'e'){
         e.preventDefault()
+
         searchInput.style.display = "flex";
         searchInput.style.animationDelay = "0.75s";
         searchInput.style.animation = "windowIntro .45s cubic-bezier(.17,.6,.22,.88)";
+
         searchInputField.focus();
+        // Then, we are checking for shortcuts inside 
+        // search
         document.addEventListener('keydown', e =>{
+
             if(e.key === 'Escape'){
                 searchInput.style.display = "none";
-            }else if(e.key === 'Enter'){
+            }
+            else if(e.key === 'Enter'){
                 searchInputValue = searchInputField.value;
                 if (searchInputValue === ''){
                     searchInputFilled = false;
-                }else{
-                    searchInputFilled = true;
-                    alert('passed');
                 }
+                else{
+                    searchInputFilled = true;
+                    
+                }
+            }
+            // If the user actually types something into the search bar, then make suggestions visible
+            else{
+                checkInput()
             }
         })
     }
